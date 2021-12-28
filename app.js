427a974fe9f058cce35f73a3119c5cf148ffbda8
youssif hany <youssifhany75@gmail.com>
2021-12-29 01:03:41 +0200

    add globar-error-handler

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -12,12 +15,22 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Hallo from middleware 👋');
-  next();
-});
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  const err = new AppError(
+    `Can't find ${req.originalUrl} on this server!`,
+    404
+  );
+
+  next(err);
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
