182abdcd75285e3e2234a69cd36d865e87793426
youssif hany <youssifhany75@gmail.com>
2022-01-03 00:27:07 +0200

    add rate-limit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 
@@ -9,20 +10,26 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+// Global middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
-
   const err = new AppError(
     `Can't find ${req.originalUrl} on this server!`,
     404
