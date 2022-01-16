04c339fa0dcff2db7efce296eecc71e79db95422
youssif hany <youssifhany75@gmail.com>
2022-01-16 23:56:16 +0200

    add bookking-crud

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,6 +5,12 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
+exports.getAllBookings = factory.getAll(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.createBooking = factory.createOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
+
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get currently booked tour
   const tour = await Tour.findById(req.params.tourId);
