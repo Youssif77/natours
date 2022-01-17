6b4d07a758fc33293ab4f8e85e75a50f729f76e2
youssif hany <youssifhany75@gmail.com>
2022-01-17 18:56:40 +0200

    add heroku-weebhook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
@@ -18,9 +19,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     mode: 'payment',
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -44,12 +43,91 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: 'success', session });
 });
 
-exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-  // This is only TEMP, because it's UNSECURE: everyone can make bookings without paying
-  const { user, tour, price } = req.query;
+// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+//   // This is only TEMP, because it's UNSECURE: everyone can make bookings without paying
+//   const { user, tour, price } = req.query;
 
-  if (!user || !tour || !price) return next();
+//   if (!user || !tour || !price) return next();
+
+//   await Booking.create({ user, tour, price });
+//   res.redirect(req.originalUrl.split('?')[0]);
+// });
+
+async function createBookingCheckout(session) {
+  console.log(session);
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total / 100;
 
   await Booking.create({ user, tour, price });
-  res.redirect(req.originalUrl.split('?')[0]);
-});
+}
+
+exports.webhookCheckout = (req, res, next) => {
+  const signature = req.headers['stripe-signature'];
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      process.env.STRIPE_WEEBHOOK_SECRET
+    );
+  } catch (err) {
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object;
+    createBookingCheckout(session);
+  }
+
+  res.status(200).json({ received: true });
+};
+
+// server.js
+//
+// Use this sample code to handle webhook events in your integration.
+//
+// 1) Paste this code into a new file (server.js)
+//
+// 2) Install dependencies
+//   npm install stripe
+//   npm install express
+//
+// 3) Run the server on http://localhost:4242
+//   node server.js
+
+// const stripe = require('stripe');
+// const express = require('express');
+// const app = express();
+
+// // This is your Stripe CLI webhook secret for testing your endpoint locally.
+
+// app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+//   const sig = request.headers['stripe-signature'];
+
+//   let event;
+
+//   try {
+//     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+//   } catch (err) {
+//     response.status(400).send(`Webhook Error: ${err.message}`);
+//     return;
+//   }
+
+//   // Handle the event
+//   switch (event.type) {
+//     case 'checkout.session.completed':
+//       const session = event.data.object;
+//       // Then define and call a function to handle the event checkout.session.completed
+//       break;
+//     // ... handle other event types
+//     default:
+//       console.log(`Unhandled event type ${event.type}`);
+//   }
+
+//   // Return a 200 response to acknowledge receipt of the event
+//   response.send();
+// });
+
+// app.listen(4242, () => console.log('Running on port 4242'));
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
 const authController = require('./../controllers/authController');
-const bookingController = require('./../controllers/bookingController');
+// const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
@@ -19,7 +19,7 @@ router.use(authController.isLoggedIn);
 
 router.get(
   '/',
-  bookingController.createBookingCheckout,
+  // bookingController.createBookingCheckout,
   viewsController.getOverview
 );
 
